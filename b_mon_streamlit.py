769cdb27148136813be19176481916d4d88baa18
Nick Weessies <152985426+nweessies@users.noreply.github.com>
2024-12-05 16:08:54 +0100

    Update b_mon_streamlit.py

--- b_mon_streamlit.py
+++ b_mon_streamlit.py
@@ -93,10 +93,7 @@ with col1:
                 smooth_factor=0
             ).add_to(m)
 
-            # Voeg hover toe voor debugging
-            for key in choropleth._children:
-                if key.startswith('color_map'):
-                    choropleth._children[key].add_to(m)
+
 
             # Toevoegen van interactieve GeoJSON laag met popup
             geojson = folium.GeoJson(
