16ed63382f6ff59a2029d9991c086b17e833e184
Nick Weessies <152985426+nweessies@users.noreply.github.com>
2024-12-09 16:20:05 +0100

    Update 2_wonen.py

--- pages/2_wonen.py
+++ pages/2_wonen.py
@@ -9,17 +9,6 @@ import geopandas as gpd
 from pyproj import transform, Proj
 
 st.title("Thema: Wonen")
-
-import pandas as pd 
-import streamlit as st
-import folium
-import numpy as np
-from streamlit_folium import st_folium
-import plotly.express as px
-import json
-import geopandas as gpd
-from pyproj import transform, Proj
-
 # Page config
 st.set_page_config(layout="wide")
 
