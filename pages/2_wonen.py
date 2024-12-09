14250af62a0a3414adff24c8e653ac240b54d74e
Nick Weessies <152985426+nweessies@users.noreply.github.com>
2024-12-09 16:17:32 +0100

    Create 2_wonen.py

---
+++ pages/2_wonen.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+import streamlit as st
+import folium
+import numpy as np
+from streamlit_folium import st_folium
+import plotly.express as px
+import json
+import geopandas as gpd
+from pyproj import transform, Proj
+
+st.title("Thema: Wonen")
